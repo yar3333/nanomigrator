714360ce799dfd9481858493ab81b74b3a0b8c0b
Yaroslav Sivakov <ys@payler.com>
2017-12-19 16:37:58 +0300

    * ConnectionData: store parsed data

--- NanoMigratorLibrary/ConnectionData.cs
+++ NanoMigratorLibrary/ConnectionData.cs
@@ -10,23 +10,44 @@ namespace NanoMigratorLibrary
 		public readonly DriverType driver;
 		public readonly string connectionString;
 
-		public string database
-		{
-			get
-			{
-				switch (driver)
-				{
-					case DriverType.MySql: return new MySqlConnectionStringBuilder(connectionString).Database;
-					case DriverType.SqlServer: return new SqlConnectionStringBuilder(connectionString).InitialCatalog;
-				}
-				throw new Exception("Unknow driver: " + driver + ".");
-			}
-		}
+		public readonly string host;
+		public readonly uint port;
+		public readonly string user;
+		public readonly string password;
+		public readonly string database;
 
 		public ConnectionData(DriverType driver, string connectionString)
 		{
 			this.driver = driver;
 			this.connectionString = connectionString;
+
+			switch (driver)
+			{
+				case DriverType.MySql:
+				{
+					var builder = new MySqlConnectionStringBuilder(connectionString);
+					host = builder.Server;
+					port = builder.Port;
+					user = builder.UserID;
+					password = builder.Password;
+					database = builder.Database;
+					break;
+				}
+				
+				case DriverType.SqlServer:
+				{
+					var builder = new SqlConnectionStringBuilder(connectionString);
+					host = builder.DataSource.Split(',')[0];
+					port = builder.DataSource.Split(',').Length >= 2 ? uint.Parse(builder.DataSource.Split(',')[1]) : 1433;
+					user = builder.UserID;
+					password = builder.Password;
+					database = builder.InitialCatalog;
+					break;
+				}
+				
+				default:
+					throw new Exception("Unknow driver: " + driver + ".");
+			}
 		}
 
 		public DbConnection createConnection()
