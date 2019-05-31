3cd11a644b9e230a1b30d4925712edfd72d87f53
Yaroslav Sivakov <yar3333@gmail.com>
2019-05-31 12:19:06 +0300

    v1.1.1

--- NanoMigratorLibrary/nuget-push.cmd
+++ NanoMigratorLibrary/nuget-push.cmd
@@ -1,1 +1,13 @@
+@ECHO OFF
+
+IF [%2]==[] GOTO :help
+
 nuget push NanoMigratorLibrary.%1.nupkg %2 -Source https://api.nuget.org/v3/index.json
+goto exit
+
+:help
+echo.
+echo Usage: nuget-push ^<version^> ^<token^>
+echo.
+
+:exit
